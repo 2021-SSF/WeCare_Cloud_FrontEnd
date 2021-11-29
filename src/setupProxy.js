261f7b3a75b6915eb2653506e18eddb393d28b55
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2021-11-29 16:46:42 +0900

    FEAT : proxy ip modified

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     '/api/facility/',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: 'http://3.134.120.190:8000',
       changeOrigin: true,
     })
   );
